52dd03619b769f167f50b253441cf18dfe86ae88
Srijan <srijanpainuly5@gmail.com>
2023-08-27 00:30:37 +0530

    sui

--- matrix.py
+++ matrix.py
@@ -14,6 +14,8 @@ def read_matrix_element():
     if matrix==[]:
         print("creating a matrix first")
         creat_matrix()
+        for i in matrix:
+            print(i)
     row_read=int(input("enter the row no. to read -->"))
     col_read=int(input("enter col no to read -->"))
     print(matrix[row_read-1][col_read-1])
@@ -26,22 +28,28 @@ def matrix_edit():
     if matrix==[]:
         print("creating a matrix first")
         creat_matrix()    
+    for i in matrix:
+        print(i)
     row_edit=int(input("enter the row no.-->"))
     col_edit=int(input("enter col no -->"))
     val=int(input("enter val no -->"))
     matrix[row_edit-1][col_edit-1]=val
+    for i in matrix:
+        print(i)
     
 while True:
-    a=int(input("enter choice from the following:\n1.create a matrix\n2.read a matrix element\n3.eddit matrix\n4.to stop\n===>"))
+    a=int(input("enter choice from the following:\n1.create a matrix\n2.read a matrix element\n3.eddit matrix\n4.to read matrix\n5.to stop\n===> "))
     if a==1:
         creat_matrix()
     if a==2:
-        for i in matrix:
-            print(i)
         read_matrix_element()
     if a==3:
-        for i in matrix:
-            print(i)
         matrix_edit()
     if a==4:
-            break
+        if matrix==[]:
+            print("creating a matrix first")
+            creat_matrix()
+        for i in matrix:
+            print(i)
+    if a==5:
+        break
