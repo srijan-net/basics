4423c08e41f781303601a6ba912a22b0e0638e12
Srijan <srijanpainuly5@gmail.com>
2023-08-31 19:32:43 +0530

    .

--- determinant of matrix.py
+++ determinant of matrix.py
@@ -1,40 +1,6 @@
+
 matrix=[[1,4,6],[5,10,7],[4,3,8]]
-tmat=[]    #first row excluded     
-first_row=[]                                          
-c=0
-for i in matrix:
-    c+=1
-    if c==1:
-        for j in i:
-            first_row.append(j)
-    if c!=1:                    #finding determinant using row 1 therfore excluding row 1 
-        tmat.append(i)
-temp_matrix=tmat
-temp_matrix2=temp_matrix
 
-
-def det3():
-    t1=[]                                             #t---> temp matrix
-    for i in tmat:
-        t1.append(i[1:3])
-    val1=det2(t1)
-    
-    t3=[]
-    for i in tmat:
-        t3.append(i[0:2])
-    val3=det2(t3)
-
-
-    t2=[]
-    for i in temp_matrix2:
-        del i[1]
-    t2=temp_matrix2
-    val2=det2(t2)
-    # print(first_row)
-    det=(val1*first_row[0])+(val2*first_row[1])+(val3*first_row[2])
-    print(det)
-    
-    
 
 
 def det2(a):
@@ -58,6 +24,45 @@ def det2(a):
     det=(r1_1*r2_2)-(r1_2*r2_1)
     return det
 
-det3()        
+def det3():
+    tmat=[]    #first row excluded     
+    first_row=[]                                          
+    c=0
+    for i in matrix:
+        c+=1
+        if c==1:
+            for j in i:
+                first_row.append(j)
+        if c!=1:                    #finding determinant using row 1 therfore excluding row 1 
+            tmat.append(i)
+    temp_matrix=tmat
+    temp_matrix2=temp_matrix    
+
+
+
+    t1=[]                                             #t---> temp matrix
+    for i in tmat:
+        t1.append(i[1:3])
+    val1=det2(t1)
+    
+    t3=[]
+    for i in tmat:
+        t3.append(i[0:2])
+    val3=det2(t3)
+
+
+    t2=[]
+    for i in temp_matrix2:
+        del i[1]
+    t2=temp_matrix2
+    val2=det2(t2)
+    # print(first_row)
+    det=(val1*first_row[0])+(val2*first_row[1])+(val3*first_row[2])
+    print(det)
+det3()
+    
+    
+
+
 
 
--- matrix.py
+++ matrix.py
@@ -1,12 +1,14 @@
 matrix=[]
 row=0
 col=0
+
 def print_matrix():
     if matrix==[]:
         print("creat a matrix first")
         creat_matrix()
     for i in matrix:
         print(i)
+
 def creat_matrix():
     row_num=int(input("enter the no. of rows "))
     col_num=int(input("enter num of colnms"))
@@ -37,13 +39,14 @@ def matrix_edit():
     matrix[row_edit-1][col_edit-1]=val
     for i in matrix:
         print(i)
-def det2():
+
+def det2(a):
     r1_1=0   #row 1 element 1
     r1_2=0   #row 1 element 2
     r2_1=0
     r2_2=0
     count=0
-    for i in matrix:
+    for i in a:
         for j in i:
             count+=1
             if count==1:
@@ -55,15 +58,43 @@ def det2():
             if count==4:
                 r2_2=j
     # print(r1_1,r1_2,r2_1,r2_2)            #just to check :)
-    determi=(r1_1*r2_2)-(r1_2*r2_1)
-    print(determi)
+    det=(r1_1*r2_2)-(r1_2*r2_1)
+    return det
 
-def det():
-    print_matrix()    
-    if row==col:
+def det3():
+    tmat=[]    #first row excluded     
+    first_row=[]                                          
+    c=0
+    for i in matrix:
+        c+=1
+        if c==1:
+            for j in i:
+                first_row.append(j)
+        if c!=1:                    #finding determinant using row 1 therfore excluding row 1 
+            tmat.append(i)
+    temp_matrix=tmat
+    temp_matrix2=temp_matrix
 
-    # else:
-        print("matrix should be a 'square matrix' to find determinant ")
+
+    t1=[]                                             #t---> temp matrix
+    for i in tmat:
+        t1.append(i[1:3])
+    val1=det2(t1)
+    
+    t3=[]
+    for i in tmat:
+        t3.append(i[0:2])
+    val3=det2(t3)
+
+    t2=[]
+    for i in temp_matrix2:
+        del i[1]
+    t2=temp_matrix2
+    val2=det2(t2)
+    # print(first_row)
+    det=(val1*first_row[0])+(val2*first_row[1])+(val3*first_row[2])
+    print(det)
+
 while True:
     a=int(input("enter choice from the following:\n1.create a matrix\n2.read a matrix element\n3.eddit matrix\n4.to read matrix\n5.to find determinant \n6.to stop\n===> "))
     if a==1:
