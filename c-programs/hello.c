8e93569d0528335ef0117b845325e077863e6120
srijan-net <srijanpainuly5@gmail.com>
2023-09-28 20:42:21 +0530

    .

--- c-programs/hello.c
+++ c-programs/hello.c
@@ -1,5 +1,13 @@
 #include<stdio.h>
-int main(){
-    printf("hello wod");
-    return 60;
+
+
+int main()
+{
+    int av;
+    scanf("%d",av);
+    printf("input hui ");
+    printf("hello world");
+    return 0;
+
 }
+
